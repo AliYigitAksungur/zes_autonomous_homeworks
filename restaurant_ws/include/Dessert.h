7e34b4741dbd2cb3ffa04af28d8e39607e9d57a3
AliYigitAksungur <71457923+AliYigitAksungur@users.noreply.github.com>
2025-10-29 17:13:00 +0300

    Create Dessert.h

---
+++ restaurant_ws/include/Dessert.h
@@ -0,0 +1,18 @@
+#ifndef DESSERT_H
+#define DESSERT_H
+
+#include "MenuItem.h"
+
+namespace menu {
+
+class Dessert : public MenuItem {
+    bool extraChocolate;
+public:
+    Dessert(std::string n = "", double p = 0.0, double tb = 0.0);
+    void customize() override;
+    void printInfo() const override;
+};
+
+}
+
+#endif
